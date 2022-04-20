6a2c5aec1bf798273059f6c959bdf8209690caca
Tyler <tec5271@psu.edu>
2022-04-20 13:04:11 -0400

    updated python files

--- Python/NLP/tyler-nlp1.py
+++ Python/NLP/tyler-nlp1.py
@@ -8,7 +8,7 @@ import spacy
 nlp = spacy.cli.download("en_core_web_sm")
 nlp = spacy.load('en_core_web_sm')
 
-avatarSpeeches = open('avatarSpeeches.txt', 'r')
+avatarSpeeches = open('avatarSpeeches.txt', 'r', encoding="utf8")
 words = avatarSpeeches.read()
 wordstrings = str(words)
 print(wordstrings)
